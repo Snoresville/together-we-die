b244031bd1fd5eb2a12a99fa821789cf7d0b51a2
Woo Huiren <giantcrabby@gmail.com>
2019-08-30 15:32:03 +0800

    Fix heart so that it gets disabled when attacked

---
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_heart_of_tarrasque_lua/item_heart_of_tarrasque_2_lua.lua
@@ -0,0 +1,8 @@
+item_heart_of_tarrasque_2_lua = class({})
+LinkLuaModifier( "modifier_item_heart_of_tarrasque_lua", "lua_abilities/item_heart_of_tarrasque_lua/modifier_item_heart_of_tarrasque_lua", LUA_MODIFIER_MOTION_NONE )
+
+--------------------------------------------------------------------------------
+-- Passive Modifier
+function item_heart_of_tarrasque_2_lua:GetIntrinsicModifierName()
+	return "modifier_item_heart_of_tarrasque_lua"
+end
---
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_heart_of_tarrasque_lua/modifier_item_heart_of_tarrasque_lua.lua
@@ -0,0 +1,79 @@
+modifier_item_heart_of_tarrasque_lua = class({})
+
+--------------------------------------------------------------------------------
+-- Classifications
+function modifier_item_heart_of_tarrasque_lua:IsHidden()
+	return true
+end
+
+function modifier_item_heart_of_tarrasque_lua:IsPurgable()
+	return false
+end
+
+--------------------------------------------------------------------------------
+-- Initializations
+function modifier_item_heart_of_tarrasque_lua:OnCreated( kv )
+	-- references
+	self.bonus_strength = self:GetAbility():GetSpecialValueFor( "bonus_strength" )
+	self.bonus_health = self:GetAbility():GetSpecialValueFor( "bonus_health" )
+	self.hp_regen_amp = self:GetAbility():GetSpecialValueFor( "hp_regen_amp" )
+	self.health_regen_rate = self:GetAbility():GetSpecialValueFor( "health_regen_rate" )
+	self.hurt_cooldown = self:GetAbility():GetSpecialValueFor( "hurt_cooldown" )
+end
+
+function modifier_item_heart_of_tarrasque_lua:OnRefresh( kv )
+
+end
+
+function modifier_item_heart_of_tarrasque_lua:OnDestroy( kv )
+
+end
+
+function modifier_item_heart_of_tarrasque_lua:GetAttributes()
+	return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+--------------------------------------------------------------------------------
+-- Modifier Effects
+function modifier_item_heart_of_tarrasque_lua:DeclareFunctions()
+	local funcs = {
+		MODIFIER_EVENT_ON_TAKEDAMAGE,
+		MODIFIER_PROPERTY_HEALTH_BONUS,
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+		MODIFIER_PROPERTY_HEALTH_REGEN_PERCENTAGE,
+		MODIFIER_PROPERTY_HP_REGEN_AMPLIFY_PERCENTAGE,
+	}
+
+	return funcs
+end
+
+function modifier_item_heart_of_tarrasque_lua:OnTakeDamage( params )
+	if IsServer() and params.unit == self:GetParent() and params.attacker ~= self:GetParent() then
+		-- cooldown
+		self:GetAbility():StartCooldown( self.hurt_cooldown )
+	end
+end
+
+function modifier_item_heart_of_tarrasque_lua:GetModifierHealthBonus( params )
+	return self.bonus_health
+end
+
+function modifier_item_heart_of_tarrasque_lua:GetModifierBonusStats_Strength( params )
+	return self.bonus_strength
+end
+
+function modifier_item_heart_of_tarrasque_lua:GetModifierHealthRegenPercentage( params )
+	if not self:GetParent():PassivesDisabled() and self:GetAbility():IsCooldownReady() then
+		return self.health_regen_rate
+	end
+
+	return 0
+end
+
+function modifier_item_heart_of_tarrasque_lua:GetModifierHPRegenAmplify_Percentage( params )
+	if not self:GetParent():PassivesDisabled() and self:GetAbility():IsCooldownReady() then
+		return self.hp_regen_amp
+	end
+
+	return 0
+end
