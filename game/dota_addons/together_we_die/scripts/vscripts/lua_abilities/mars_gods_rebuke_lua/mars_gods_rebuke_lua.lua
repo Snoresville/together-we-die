8e8941cb7b2cbfc5fe1094419d36eb47224c3534
Woo Huiren <giantcrabby@gmail.com>
2019-08-31 19:43:04 +0800

    Add scalable Mars gods rebuke

---
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/mars_gods_rebuke_lua/mars_gods_rebuke_lua.lua
@@ -0,0 +1,159 @@
+-- Created by Elfansoer
+--[[
+Ability checklist (erase if done/checked):
+- Scepter Upgrade
+- Break behavior
+- Linken/Reflect behavior
+- Spell Immune/Invulnerable/Invisible behavior
+- Illusion behavior
+- Stolen behavior
+]]
+--------------------------------------------------------------------------------
+mars_gods_rebuke_lua = class({})
+LinkLuaModifier( "modifier_mars_gods_rebuke_lua", "lua_abilities/mars_gods_rebuke_lua/modifier_mars_gods_rebuke_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_generic_knockback_lua", "lua_abilities/generic/modifier_generic_knockback_lua", LUA_MODIFIER_MOTION_BOTH )
+
+--------------------------------------------------------------------------------
+-- Ability Start
+function mars_gods_rebuke_lua:OnSpellStart()
+	-- unit identifier
+	local caster = self:GetCaster()
+	local point = self:GetCursorPosition()
+
+	-- load data
+	local radius = self:GetSpecialValueFor("radius")
+	local angle = self:GetSpecialValueFor("angle")/2
+	local duration = self:GetSpecialValueFor("knockback_duration")
+	local distance = self:GetSpecialValueFor("knockback_distance")
+
+	-- find units
+	local enemies = FindUnitsInRadius(
+		caster:GetTeamNumber(),	-- int, your team number
+		caster:GetOrigin(),	-- point, center point
+		nil,	-- handle, cacheUnit. (not known)
+		radius,	-- float, radius. or use FIND_UNITS_EVERYWHERE
+		DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
+		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
+		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,	-- int, flag filter
+		0,	-- int, order filter
+		false	-- bool, can grow cache
+	)
+
+	-- add buff modifier
+	local buff = caster:AddNewModifier(
+		caster, -- player source
+		self, -- ability source
+		"modifier_mars_gods_rebuke_lua", -- modifier name
+		{  } -- kv
+	)
+
+	-- precache
+	local origin = caster:GetOrigin()
+	local cast_direction = (point-origin):Normalized()
+	local cast_angle = VectorToAngles( cast_direction ).y
+	local damageTable = {
+		attacker = caster,
+		damage = caster:GetStrength() * self:GetSpecialValueFor("str_multiplier"),
+		damage_type = self:GetAbilityDamageType(),
+		ability = self, --Optional.
+		damage_flags = DOTA_DAMAGE_FLAG_NONE, --Optional.
+	}
+
+	-- for each units
+	local caught = false
+	for _,enemy in pairs(enemies) do
+		-- check within cast angle
+		local enemy_direction = (enemy:GetOrigin() - origin):Normalized()
+		local enemy_angle = VectorToAngles( enemy_direction ).y
+		local angle_diff = math.abs( AngleDiff( cast_angle, enemy_angle ) )
+		if angle_diff<=angle then
+			-- attack
+			caster:PerformAttack(
+				enemy,
+				true,
+				true,
+				true,
+				true,
+				true,
+				false,
+				true
+			)
+
+			-- apply damage
+			damageTable.victim = enemy
+			ApplyDamage(damageTable)
+
+			-- knockback if not having spear stun
+			if not enemy:HasModifier( "modifier_mars_spear_of_mars_lua_debuff" ) then
+				enemy:AddNewModifier(
+					caster, -- player source
+					self, -- ability source
+					"modifier_generic_knockback_lua", -- modifier name
+					{
+						duration = duration,
+						distance = distance,
+						height = 30,
+						direction_x = enemy_direction.x,
+						direction_y = enemy_direction.y,
+					} -- kv
+				)
+			end
+
+			caught = true
+			-- play effects
+			self:PlayEffects2( enemy, origin, cast_direction )
+		end
+	end
+
+	-- destroy buff modifier
+	buff:Destroy()
+
+	-- play effects
+	self:PlayEffects1( caught, (point-origin):Normalized() )
+end
+
+--------------------------------------------------------------------------------
+
+function mars_gods_rebuke_lua:GetCooldown( iLevel )
+	if self:GetCaster():HasScepter() then
+		return self:GetSpecialValueFor( "cooldown_scepter" )
+	else
+		return self:GetSpecialValueFor( "cooldown" )
+	end
+end
+
+--------------------------------------------------------------------------------
+-- Play Effects
+function mars_gods_rebuke_lua:PlayEffects1( caught, direction )
+	-- Get Resources
+	local particle_cast = "particles/units/heroes/hero_mars/mars_shield_bash.vpcf"
+	local sound_cast = "Hero_Mars.Shield.Cast"
+	if not caught then
+		local sound_cast = "Hero_Mars.Shield.Cast.Small"
+	end
+
+	-- Create Particle
+	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_WORLDORIGIN, self:GetCaster() )
+	ParticleManager:SetParticleControl( effect_cast, 0, self:GetCaster():GetOrigin() )
+	ParticleManager:SetParticleControlForward( effect_cast, 0, direction )
+	ParticleManager:ReleaseParticleIndex( effect_cast )
+
+	-- Create Sound
+	EmitSoundOnLocationWithCaster( self:GetCaster():GetOrigin(), sound_cast, self:GetCaster() )
+end
+
+function mars_gods_rebuke_lua:PlayEffects2( target, origin, direction )
+	-- Get Resources
+	local particle_cast = "particles/units/heroes/hero_mars/mars_shield_bash_crit.vpcf"
+	local sound_cast = "Hero_Mars.Shield.Crit"
+
+	-- Create Particle
+	local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_WORLDORIGIN, target )
+	ParticleManager:SetParticleControl( effect_cast, 0, target:GetOrigin() )
+	ParticleManager:SetParticleControl( effect_cast, 1, target:GetOrigin() )
+	ParticleManager:SetParticleControlForward( effect_cast, 1, direction )
+	ParticleManager:ReleaseParticleIndex( effect_cast )
+
+	-- Create Sound
+	EmitSoundOn( sound_cast, target )
+end
