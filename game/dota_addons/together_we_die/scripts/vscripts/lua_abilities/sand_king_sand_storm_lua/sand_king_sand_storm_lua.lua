1e9c5a7e4b2618bcb51e081fdb8849a33a9b5722
Woo Huiren <giantcrabby@gmail.com>
2019-09-10 21:15:10 +0800

    Change sandstorm to non-channeling

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/sand_king_sand_storm_lua/sand_king_sand_storm_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/sand_king_sand_storm_lua/sand_king_sand_storm_lua.lua
@@ -7,15 +7,12 @@ function sand_king_sand_storm_lua:OnSpellStart()
 	-- unit identifier
 	local caster = self:GetCaster()
 
-	-- load data
-	local duration = self:GetSpecialValueFor("AbilityDuration")
-
 	caster:AddNewModifier(
 		caster, -- player source
 		self, -- ability source
 		"modifier_sand_king_sand_storm_lua", -- modifier name
 		{
-			duration = duration,
+			duration = self:GetDuration(),
 			start = true,
 		} -- kv
 	)
@@ -23,23 +20,4 @@ function sand_king_sand_storm_lua:OnSpellStart()
 	-- effects
 	local sound_cast = "Ability.SandKing_SandStorm.start"
 	EmitSoundOn( sound_cast, caster )
-end
-
---------------------------------------------------------------------------------
--- Ability Channeling
--- function sand_king_sand_storm_lua:GetChannelTime()
-
--- end
-
-function sand_king_sand_storm_lua:OnChannelFinish( bInterrupted )
-	local delay = self:GetSpecialValueFor("sand_storm_invis_delay")
-	self:GetCaster():AddNewModifier(
-		self:GetCaster(), -- player source
-		self, -- ability source
-		"modifier_sand_king_sand_storm_lua", -- modifier name
-		{
-			duration = delay,
-			start = false,
-		} -- kv
-	)
 end
