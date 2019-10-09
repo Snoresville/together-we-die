32c99fdf8dced663a9b8635a13c8fed22a9b6d63
Woo Huiren <giantcrabby@gmail.com>
2019-10-09 18:54:21 +0800

    Increase damage by meat hook

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/pudge_meat_hook_lua/modifier_pudge_meat_hook_followthrough_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/pudge_meat_hook_lua/modifier_pudge_meat_hook_followthrough_lua.lua
@@ -6,29 +6,11 @@ function modifier_pudge_meat_hook_followthrough_lua:IsHidden()
 	return true
 end
 
-function modifier_pudge_meat_hook_followthrough_lua:IsDebuff()
-	return true
-end
-
-function modifier_pudge_meat_hook_followthrough_lua:IsStunDebuff()
-	return true
-end
-
-function modifier_pudge_meat_hook_followthrough_lua:IsPurgable()
-	return true
-end
-
-function modifier_pudge_meat_hook_followthrough_lua:GetPriority()
-	return MODIFIER_PRIORITY_HIGH
-end
-
 --------------------------------------------------------------------------------
 
 function modifier_pudge_meat_hook_followthrough_lua:CheckState()
 	local state = {
-		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
 		[MODIFIER_STATE_STUNNED] = true,
-		[MODIFIER_STATE_INVISIBLE] = false,
 	}
 
 	return state
