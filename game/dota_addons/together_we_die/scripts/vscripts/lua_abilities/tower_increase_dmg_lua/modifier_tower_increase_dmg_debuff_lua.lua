6ae6e6cab3e825f9fa83423236dfc1941135f669
Woo Huiren <giantcrabby@gmail.com>
2019-12-08 20:22:29 +0800

    Improve damage stacking further

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/tower_increase_dmg_lua/modifier_tower_increase_dmg_debuff_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/tower_increase_dmg_lua/modifier_tower_increase_dmg_debuff_lua.lua
@@ -16,10 +16,29 @@ end
 --------------------------------------------------------------------------------
 
 function modifier_tower_increase_dmg_debuff_lua:OnCreated( kv )
+	self.mana_loss_per_stack = self:GetAbility():GetSpecialValueFor("mana_loss_per_stack")
 	self:SetStackCount(1)
 end
 
 function modifier_tower_increase_dmg_debuff_lua:OnRefresh( kv )
+	self.mana_loss_per_stack = self:GetAbility():GetSpecialValueFor("mana_loss_per_stack")
+end
+
+function modifier_tower_increase_dmg_debuff_lua:GetModifierConstantManaRegen()
+	return -(self:GetStackCount() * self.mana_loss_per_stack)
+end
+
+function modifier_tower_increase_dmg_debuff_lua:GetDisableHealing( params )
+	return 1
+end
+
+function modifier_tower_increase_dmg_debuff_lua:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_DISABLE_HEALING,
+		MODIFIER_PROPERTY_MANA_REGEN_CONSTANT,
+	}
+
+	return funcs
 end
 
 --------------------------------------------------------------------------------
