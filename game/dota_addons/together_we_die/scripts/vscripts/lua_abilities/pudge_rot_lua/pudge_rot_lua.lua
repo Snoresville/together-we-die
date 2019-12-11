d8ea687164f133398704a6042ad9f4109d252c73
Woo Huiren <giantcrabby@gmail.com>
2019-12-12 01:49:28 +0800

    Buff Pudge rot damage

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/pudge_rot_lua/pudge_rot_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/pudge_rot_lua/pudge_rot_lua.lua
@@ -1,27 +1,25 @@
 pudge_rot_lua = class({})
-LinkLuaModifier( "modifier_pudge_rot_lua", "lua_abilities/pudge_rot_lua/modifier_pudge_rot_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_pudge_rot_lua", "lua_abilities/pudge_rot_lua/modifier_pudge_rot_lua", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 
 function pudge_rot_lua:ProcsMagicStick()
-	return false
+    return false
 end
 
 --------------------------------------------------------------------------------
 
 function pudge_rot_lua:OnToggle()
-	if self:GetToggleState() then
-		self:GetCaster():AddNewModifier( self:GetCaster(), self, "modifier_pudge_rot_lua", nil )
+    if self:GetToggleState() then
+        self:GetCaster():AddNewModifier(self:GetCaster(), self, "modifier_pudge_rot_lua", nil)
 
-		if not self:GetCaster():IsChanneling() then
-			self:GetCaster():StartGesture( ACT_DOTA_CAST_ABILITY_ROT )
-		end
-	else
-		local hRotBuff = self:GetCaster():FindModifierByName( "modifier_pudge_rot_lua" )
-		if hRotBuff ~= nil then
-			hRotBuff:Destroy()
-		end
-	end
+        if not self:GetCaster():IsChanneling() then
+            self:GetCaster():StartGesture(ACT_DOTA_CAST_ABILITY_ROT)
+        end
+    else
+        local hRotBuff = self:GetCaster():FindModifierByName("modifier_pudge_rot_lua")
+        if hRotBuff ~= nil then
+            hRotBuff:Destroy()
+        end
+    end
 end
-
---------------------------------------------------------------------------------
