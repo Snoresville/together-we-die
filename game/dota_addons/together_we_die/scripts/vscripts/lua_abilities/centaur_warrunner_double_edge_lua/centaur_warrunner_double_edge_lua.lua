813a0a82a44fd1f2b660590ff9ee1dc8c2869083
Woo Huiren <giantcrabby@gmail.com>
2019-12-17 23:04:03 +0800

    Add centaur warruner abilities

---
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/centaur_warrunner_double_edge_lua/centaur_warrunner_double_edge_lua.lua
@@ -0,0 +1,91 @@
+centaur_warrunner_double_edge_lua = class({})
+
+--------------------------------------------------------------------------------
+-- Ability Start
+function centaur_warrunner_double_edge_lua:OnSpellStart()
+    -- unit identifier
+    local caster = self:GetCaster()
+    local target = self:GetCursorTarget()
+
+    -- cancel if linken
+    if target:TriggerSpellAbsorb(self) then
+        return
+    end
+
+    -- load data
+    local damage = self:GetSpecialValueFor("edge_damage") + caster:GetStrength() * self:GetSpecialValueFor("str_multiplier")
+    local radius = self:GetSpecialValueFor("radius")
+
+    -- Find Units in Radius
+    local enemies = FindUnitsInRadius(
+            self:GetCaster():GetTeamNumber(), -- int, your team number
+            target:GetOrigin(), -- point, center point
+            nil, -- handle, cacheUnit. (not known)
+            radius, -- float, radius. or use FIND_UNITS_EVERYWHERE
+            DOTA_UNIT_TARGET_TEAM_ENEMY, -- int, team filter
+            DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, -- int, type filter
+            0, -- int, flag filter
+            0, -- int, order filter
+            false    -- bool, can grow cache
+    )
+
+    -- Precache damage
+    local damageTable = {
+        -- victim = target,
+        attacker = caster,
+        damage = damage,
+        damage_type = self:GetAbilityDamageType(),
+        ability = self, --Optional.
+    }
+
+    -- Apply aoe damage
+    for _, enemy in pairs(enemies) do
+        damageTable.victim = enemy
+        ApplyDamage(damageTable)
+    end
+
+    -- Apply self-damage
+    damageTable.victim = caster
+    damageTable.damage_flags = DOTA_DAMAGE_FLAG_NON_LETHAL
+    ApplyDamage(damageTable)
+
+    -- Play effects
+    self:PlayEffects(target)
+end
+
+--------------------------------------------------------------------------------
+function centaur_warrunner_double_edge_lua:PlayEffects(target)
+    -- Get Resources
+    local particle_cast = "particles/units/heroes/hero_centaur/centaur_double_edge.vpcf"
+    local sound_cast = "Hero_Centaur.DoubleEdge"
+
+    -- Get Data
+    local forward = (target:GetOrigin() - self:GetCaster():GetOrigin()):Normalized()
+
+    -- Create Particle
+    local effect_cast = ParticleManager:CreateParticle(particle_cast, PATTACH_ABSORIGIN_FOLLOW, target)
+    ParticleManager:SetParticleControlEnt(
+            effect_cast,
+            0,
+            self:GetCaster(),
+            PATTACH_POINT_FOLLOW,
+            "attach_hitloc",
+            self:GetCaster():GetOrigin(), -- unknown
+            true -- unknown, true
+    )
+    ParticleManager:SetParticleControlEnt(
+            effect_cast,
+            1,
+            target,
+            PATTACH_POINT_FOLLOW,
+            "attach_hitloc",
+            target:GetOrigin(), -- unknown
+            true -- unknown, true
+    )
+    ParticleManager:SetParticleControlForward(effect_cast, 2, forward)
+    ParticleManager:SetParticleControlForward(effect_cast, 5, forward)
+    ParticleManager:ReleaseParticleIndex(effect_cast)
+
+    -- Create Sound
+    EmitSoundOn(sound_cast, target)
+end
