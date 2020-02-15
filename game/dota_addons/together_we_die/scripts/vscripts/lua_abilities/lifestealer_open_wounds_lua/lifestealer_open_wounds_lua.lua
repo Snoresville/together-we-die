3cbb4ddb464e8f3633260a1fbaf43f6e3eea7d4d
Woo Huiren <giantcrabby@gmail.com>
2020-02-15 21:05:50 +0800

    Change open wounds to AOE + add english

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lifestealer_open_wounds_lua/lifestealer_open_wounds_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lifestealer_open_wounds_lua/lifestealer_open_wounds_lua.lua
@@ -2,25 +2,44 @@ lifestealer_open_wounds_lua = class({})
 LinkLuaModifier( "modifier_lifestealer_open_wounds_lua", "lua_abilities/lifestealer_open_wounds_lua/modifier_lifestealer_open_wounds_lua", LUA_MODIFIER_MOTION_NONE )
 
 --------------------------------------------------------------------------------
+-- AOE Radius
+function lifestealer_open_wounds_lua:GetAOERadius()
+	return self:GetSpecialValueFor( "radius" )
+end
 -- Ability Start
 function lifestealer_open_wounds_lua:OnSpellStart()
 	-- unit identifier
 	local caster = self:GetCaster()
 	local target = self:GetCursorTarget()
 
-	-- cancel if linken
-	if target:TriggerSpellAbsorb( self ) then return end
-
 	-- load data
 	local duration = self:GetSpecialValueFor("duration")
 
-	-- apply modifier
-	target:AddNewModifier(
-		caster, -- player source
-		self, -- ability source
-		"modifier_lifestealer_open_wounds_lua", -- modifier name
-		{ duration = duration } -- kv
+	local search = self:GetSpecialValueFor( "radius" )
+	local targets = FindUnitsInRadius(
+			caster:GetTeamNumber(),	-- int, your team number
+			target:GetOrigin(),	-- point, center point
+			target,	-- handle, cacheUnit. (not known)
+			search,	-- float, radius. or use FIND_UNITS_EVERYWHERE
+			DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
+			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
+			0,	-- int, flag filter
+			0,	-- int, order filter
+			false	-- bool, can grow cache
 	)
+
+	for _,enemy in pairs(targets) do
+		-- cancel if linken
+		if not enemy:TriggerSpellAbsorb( self ) then
+			-- add modifier
+			enemy:AddNewModifier(
+					caster, -- player source
+					self, -- ability source
+					"modifier_lifestealer_open_wounds_lua", -- modifier name
+					{ duration = duration } -- kv
+			)
+		end
+	end
 
 	-- play effects
 	local sound_cast = "Hero_LifeStealer.OpenWounds.Cast"
