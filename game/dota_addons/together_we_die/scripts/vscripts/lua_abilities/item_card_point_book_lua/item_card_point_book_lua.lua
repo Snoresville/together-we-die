e33873746dd254726e7b85c1ca75dce6e05564e4
Woo Huiren <giantcrabby@gmail.com>
2020-05-02 14:28:31 +0800

    Add check to make sure is a real hero

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_card_point_book_lua/item_card_point_book_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_card_point_book_lua/item_card_point_book_lua.lua
@@ -8,17 +8,19 @@ function item_card_point_book_lua:OnSpellStart()
     local cardPointAbility
     local cardPoints = 0
 
-    -- check for card points and retrieve
-    if caster:HasAbility("card_points_lua") then
-        cardPointAbility = caster:FindAbilityByName("card_points_lua")
-        cardPoints = cardPointAbility:GetCP()
-    else
-        cardPointAbility = caster:AddAbility("card_points_lua")
-        cardPointAbility:SetLevel(1)
+    if caster:IsRealHero() then
+        -- check for card points and retrieve
+        if caster:HasAbility("card_points_lua") then
+            cardPointAbility = caster:FindAbilityByName("card_points_lua")
+            cardPoints = cardPointAbility:GetCP()
+        else
+            cardPointAbility = caster:AddAbility("card_points_lua")
+            cardPointAbility:SetLevel(1)
+        end
+
+        cardPointAbility:SetCP(cardPoints + cardPointsToGive)
+
+        -- Consume one charge
+        self:SpendCharge()
     end
-
-    cardPointAbility:SetCP(cardPoints + cardPointsToGive)
-
-    -- Consume one charge
-    self:SpendCharge()
 end
