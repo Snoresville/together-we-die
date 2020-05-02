e7ba852bd2a4d3b591c2c29557636c7c92dbc14e
Woo Huiren <giantcrabby@gmail.com>
2020-05-02 14:24:33 +0800

    Add card point book

---
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_card_point_book_lua/item_card_point_book_lua.lua
@@ -0,0 +1,24 @@
+item_card_point_book_lua = class({})
+
+-- Ability Start
+function item_card_point_book_lua:OnSpellStart()
+    -- unit identifier
+    local caster = self:GetCaster()
+    local cardPointsToGive = self:GetSpecialValueFor("card_points")
+    local cardPointAbility
+    local cardPoints = 0
+
+    -- check for card points and retrieve
+    if caster:HasAbility("card_points_lua") then
+        cardPointAbility = caster:FindAbilityByName("card_points_lua")
+        cardPoints = cardPointAbility:GetCP()
+    else
+        cardPointAbility = caster:AddAbility("card_points_lua")
+        cardPointAbility:SetLevel(1)
+    end
+
+    cardPointAbility:SetCP(cardPoints + cardPointsToGive)
+
+    -- Consume one charge
+    self:SpendCharge()
+end
