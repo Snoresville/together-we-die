e315ffd8597cf93b3722bf3677e87f94382d553f
Woo Huiren <giantcrabby@gmail.com>
2020-05-14 05:53:23 +0800

    Change LC duel damage to be able to passive disable

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/legion_commander_duel_lua/modifier_legion_commander_duel_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/legion_commander_duel_lua/modifier_legion_commander_duel_lua.lua
@@ -2,65 +2,69 @@ modifier_legion_commander_duel_lua = class({})
 
 --------------------------------------------------------------------------------
 function modifier_legion_commander_duel_lua:IsHidden()
-	return false
+    return false
 end
 
 function modifier_legion_commander_duel_lua:IsDebuff()
-	return false
+    return false
 end
 
 function modifier_legion_commander_duel_lua:IsPurgable()
-	return false
+    return false
 end
 
 --------------------------------------------------------------------------------
 
-function modifier_legion_commander_duel_lua:OnCreated( kv )
-	self.damage_bonus = self:GetAbility():GetSpecialValueFor( "damage_bonus" )
-	self.str_multiplier = self:GetAbility():GetSpecialValueFor( "str_multiplier" )
-	if IsServer() then
-		self:SetStackCount( self:GetAbility():GetDuelKills() )
-	end
+function modifier_legion_commander_duel_lua:OnCreated(kv)
+    self.damage_bonus = self:GetAbility():GetSpecialValueFor("damage_bonus")
+    self.str_multiplier = self:GetAbility():GetSpecialValueFor("str_multiplier")
+    if IsServer() then
+        self:SetStackCount(self:GetAbility():GetDuelKills())
+    end
 end
 
 --------------------------------------------------------------------------------
 
-function modifier_legion_commander_duel_lua:OnRefresh( kv )
-	self.damage_bonus = self:GetAbility():GetSpecialValueFor( "damage_bonus" )
-	self.str_multiplier = self:GetAbility():GetSpecialValueFor( "str_multiplier" )
+function modifier_legion_commander_duel_lua:OnRefresh(kv)
+    self.damage_bonus = self:GetAbility():GetSpecialValueFor("damage_bonus")
+    self.str_multiplier = self:GetAbility():GetSpecialValueFor("str_multiplier")
 end
 
 --------------------------------------------------------------------------------
 
 function modifier_legion_commander_duel_lua:DeclareFunctions()
-	local funcs = {
-		MODIFIER_EVENT_ON_DEATH,
-		MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE
-	}
+    local funcs = {
+        MODIFIER_EVENT_ON_DEATH,
+        MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE
+    }
 
-	return funcs
+    return funcs
 end
 
-function modifier_legion_commander_duel_lua:OnDeath( event )
-	if event.unit == nil or event.attacker == nil or event.unit:IsNull() or event.attacker:IsNull() then
-		return
-	end
+function modifier_legion_commander_duel_lua:OnDeath(event)
+    if event.unit == nil or event.attacker == nil or event.unit:IsNull() or event.attacker:IsNull() then
+        return
+    end
 
-	if event.unit:GetTeamNumber() ~= self:GetParent():GetTeamNumber() and self:GetParent():IsAlive() and event.attacker == self:GetParent() then
-		self:GetAbility():IncrementDuelKills()
+    if event.unit:GetTeamNumber() ~= self:GetParent():GetTeamNumber() and self:GetParent():IsAlive() and not self:GetParent():PassivesDisabled() and event.attacker == self:GetParent() then
+        self:GetAbility():IncrementDuelKills()
 
-		self:SetStackCount( self:GetAbility():GetDuelKills() )
+        self:SetStackCount(self:GetAbility():GetDuelKills())
 
-		local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_legion_commander/legion_commander_duel_victory.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetParent() )
-		ParticleManager:SetParticleControl( nFXIndex, 1, Vector( 1, 0, 0 ) )
-		ParticleManager:ReleaseParticleIndex( nFXIndex )
-	end
+        local nFXIndex = ParticleManager:CreateParticle("particles/units/heroes/hero_legion_commander/legion_commander_duel_victory.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetParent())
+        ParticleManager:SetParticleControl(nFXIndex, 1, Vector(1, 0, 0))
+        ParticleManager:ReleaseParticleIndex(nFXIndex)
+    end
 end
 
 --------------------------------------------------------------------------------
 
-function modifier_legion_commander_duel_lua:GetModifierPreAttack_BonusDamage( params )
-	return self:GetStackCount() * self.damage_bonus + self:GetStackCount() * self.str_multiplier * self:GetParent():GetStrength()
+function modifier_legion_commander_duel_lua:GetModifierPreAttack_BonusDamage(params)
+    if not self:GetParent():PassivesDisabled() then
+        return self:GetStackCount() * self.damage_bonus + self:GetStackCount() * self.str_multiplier * self:GetParent():GetStrength()
+    end
+
+    return 0
 end
 
 --------------------------------------------------------------------------------
