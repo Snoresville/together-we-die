3522b9f8e227db6a062231036277280e87ea14b2
Woo Huiren <giantcrabby@gmail.com>
2020-05-15 18:09:13 +0800

    Nerf global silence

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/silencer_global_silence_lua/silencer_global_silence_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/silencer_global_silence_lua/silencer_global_silence_lua.lua
@@ -10,106 +10,98 @@ Ability checklist (erase if done/checked):
 ]]
 --------------------------------------------------------------------------------
 silencer_global_silence_lua = class({})
-LinkLuaModifier( "modifier_generic_silenced_lua", "lua_abilities/generic/modifier_generic_silenced_lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_generic_break_lua", "lua_abilities/generic/modifier_generic_break_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_generic_silenced_lua", "lua_abilities/generic/modifier_generic_silenced_lua", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 function silencer_global_silence_lua:GetManaCost()
-	return self:GetCaster():GetMaxMana() * (self:GetSpecialValueFor("mana_cost") / 100) + self:GetSpecialValueFor("base_mana_cost")
+    return self:GetCaster():GetMaxMana() * (self:GetSpecialValueFor("mana_cost") / 100) + self:GetSpecialValueFor("base_mana_cost")
 end
 
 --------------------------------------------------------------------------------
 -- Ability Start
 function silencer_global_silence_lua:OnSpellStart()
-	-- unit identifier
-	local caster = self:GetCaster()
+    -- unit identifier
+    local caster = self:GetCaster()
 
-	-- load data
-	local duration = self:GetSpecialValueFor( "tooltip_duration" ) + (caster:GetIntellect() * self:GetSpecialValueFor( "int_multiplier" ))
+    -- load data
+    local duration = self:GetSpecialValueFor("tooltip_duration") + (caster:GetIntellect() * self:GetSpecialValueFor("int_multiplier"))
 
-	-- find enemies
-	local enemies = FindUnitsInRadius(
-		caster:GetTeamNumber(),	-- int, your team number
-		caster:GetOrigin(),	-- point, center point
-		nil,	-- handle, cacheUnit. (not known)
-		FIND_UNITS_EVERYWHERE,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-		DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_MANA_ONLY,	-- int, flag filter
-		0,	-- int, order filter
-		false	-- bool, can grow cache
-	)
+    -- find enemies
+    local enemies = FindUnitsInRadius(
+            caster:GetTeamNumber(), -- int, your team number
+            caster:GetOrigin(), -- point, center point
+            nil, -- handle, cacheUnit. (not known)
+            FIND_UNITS_EVERYWHERE, -- float, radius. or use FIND_UNITS_EVERYWHERE
+            DOTA_UNIT_TARGET_TEAM_ENEMY, -- int, team filter
+            DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC, -- int, type filter
+            DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_MANA_ONLY, -- int, flag filter
+            0, -- int, order filter
+            false    -- bool, can grow cache
+    )
 
-	for _,enemy in pairs(enemies) do
-		-- silence
-		enemy:AddNewModifier(
-			caster, -- player source
-			self, -- ability source
-			"modifier_generic_silenced_lua", -- modifier name
-			{ duration = duration } -- kv
-		)
+    for _, enemy in pairs(enemies) do
+        -- silence
+        enemy:AddNewModifier(
+                caster, -- player source
+                self, -- ability source
+                "modifier_generic_silenced_lua", -- modifier name
+                { duration = duration } -- kv
+        )
 
-		enemy:AddNewModifier(
-			caster, -- player source
-			self, -- ability source
-			"modifier_generic_break_lua", -- modifier name
-			{ duration = duration } -- kv
-		)
+        -- play effects
+        if enemy:IsHero() then
+            self:PlayEffects2(enemy)
+        end
+    end
 
-		-- play effects
-		if enemy:IsHero() then
-			self:PlayEffects2( enemy )
-		end
-	end
-
-	-- play effects
-	self:PlayEffects1()
+    -- play effects
+    self:PlayEffects1()
 end
 
 --------------------------------------------------------------------------------
 function silencer_global_silence_lua:PlayEffects1()
-	-- Get Resources
-	local particle_cast = "particles/units/heroes/hero_silencer/silencer_global_silence.vpcf"
-	local sound_cast = "Hero_Silencer.GlobalSilence.Cast"
+    -- Get Resources
+    local particle_cast = "particles/units/heroes/hero_silencer/silencer_global_silence.vpcf"
+    local sound_cast = "Hero_Silencer.GlobalSilence.Cast"
 
-	-- Create Particle
-	-- local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetCaster() )
-	local effect_cast = assert(loadfile("lua_abilities/rubick_spell_steal_lua/rubick_spell_steal_lua_arcana"))(self, particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetCaster() )
-	ParticleManager:SetParticleControlForward( effect_cast, 0, self:GetCaster():GetForwardVector() )
-	ParticleManager:SetParticleControlEnt(
-		effect_cast,
-		1,
-		self:GetCaster(),
-		PATTACH_POINT_FOLLOW,
-		"attach_attack1",
-		Vector(0,0,0), -- unknown
-		true -- unknown, true
-	)
-	ParticleManager:ReleaseParticleIndex( effect_cast )
+    -- Create Particle
+    -- local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetCaster() )
+    local effect_cast = assert(loadfile("lua_abilities/rubick_spell_steal_lua/rubick_spell_steal_lua_arcana"))(self, particle_cast, PATTACH_ABSORIGIN_FOLLOW, self:GetCaster())
+    ParticleManager:SetParticleControlForward(effect_cast, 0, self:GetCaster():GetForwardVector())
+    ParticleManager:SetParticleControlEnt(
+            effect_cast,
+            1,
+            self:GetCaster(),
+            PATTACH_POINT_FOLLOW,
+            "attach_attack1",
+            Vector(0, 0, 0), -- unknown
+            true -- unknown, true
+    )
+    ParticleManager:ReleaseParticleIndex(effect_cast)
 
-	-- Create Sound
-	EmitGlobalSound( sound_cast )
+    -- Create Sound
+    EmitGlobalSound(sound_cast)
 end
 
-function silencer_global_silence_lua:PlayEffects2( target )
-	-- Get Resources
-	local particle_cast = "particles/units/heroes/hero_silencer/silencer_global_silence_hero.vpcf"
-	local sound_cast = "Hero_Silencer.GlobalSilence.Effect"
+function silencer_global_silence_lua:PlayEffects2(target)
+    -- Get Resources
+    local particle_cast = "particles/units/heroes/hero_silencer/silencer_global_silence_hero.vpcf"
+    local sound_cast = "Hero_Silencer.GlobalSilence.Effect"
 
-	-- Create Particle
-	-- local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, target )
-	local effect_cast = assert(loadfile("lua_abilities/rubick_spell_steal_lua/rubick_spell_steal_lua_arcana"))(self, particle_cast, PATTACH_ABSORIGIN_FOLLOW, target )
-	ParticleManager:SetParticleControlEnt(
-		effect_cast,
-		1,
-		target,
-		PATTACH_ABSORIGIN_FOLLOW,
-		"attach_attack1",
-		Vector(0,0,0), -- unknown
-		true -- unknown, true
-	)
-	ParticleManager:ReleaseParticleIndex( effect_cast )
+    -- Create Particle
+    -- local effect_cast = ParticleManager:CreateParticle( particle_cast, PATTACH_ABSORIGIN_FOLLOW, target )
+    local effect_cast = assert(loadfile("lua_abilities/rubick_spell_steal_lua/rubick_spell_steal_lua_arcana"))(self, particle_cast, PATTACH_ABSORIGIN_FOLLOW, target)
+    ParticleManager:SetParticleControlEnt(
+            effect_cast,
+            1,
+            target,
+            PATTACH_ABSORIGIN_FOLLOW,
+            "attach_attack1",
+            Vector(0, 0, 0), -- unknown
+            true -- unknown, true
+    )
+    ParticleManager:ReleaseParticleIndex(effect_cast)
 
-	-- Create Sound
-	EmitSoundOnClient( sound_cast, target:GetPlayerOwner() )
+    -- Create Sound
+    EmitSoundOnClient(sound_cast, target:GetPlayerOwner())
 end
