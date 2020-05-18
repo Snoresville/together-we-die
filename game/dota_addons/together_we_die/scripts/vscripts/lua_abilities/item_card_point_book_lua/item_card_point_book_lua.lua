dd7ab6538b7ef0bb91c9ca210ffd8ccfb9d52e19
Woo Huiren <giantcrabby@gmail.com>
2020-05-19 00:28:43 +0800

    Fix super illusion getting card points

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_card_point_book_lua/item_card_point_book_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/item_card_point_book_lua/item_card_point_book_lua.lua
@@ -6,8 +6,14 @@ function item_card_point_book_lua:OnSpellStart()
     local caster = self:GetCaster()
     local cardPointsToGive = self:GetSpecialValueFor("card_points")
 
-    if caster:IsRealHero() then
-        holdout_card_points:_BuyCardPoints(caster:GetPlayerID(), cardPointsToGive)
+    if caster:IsHero() and caster:IsRealHero() and not caster:IsIllusion() then
+        local nPlayerID = caster:GetPlayerID()
+        local player = PlayerResource:GetPlayer(nPlayerID)
+        local playerHero = player:GetAssignedHero()
+        if caster == playerHero then
+            holdout_card_points:_BuyCardPoints(caster:GetPlayerID(), cardPointsToGive)
+        end
+
         -- Consume one charge
         self:SpendCharge()
     end
