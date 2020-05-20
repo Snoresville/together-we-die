cc669f11ffe7226f6ea7afdbc50880d5e78adf47
Woo Huiren <giantcrabby@gmail.com>
2020-05-20 22:27:28 +0800

    Fix stack counting on finger of death

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lion_finger_of_death_lua/modifier_lion_finger_of_death_lua_stack.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lion_finger_of_death_lua/modifier_lion_finger_of_death_lua_stack.lua
@@ -3,28 +3,30 @@ modifier_lion_finger_of_death_lua_stack = class({})
 --------------------------------------------------------------------------------
 -- Classifications
 function modifier_lion_finger_of_death_lua_stack:IsHidden()
-	return false
+    return false
 end
 
 function modifier_lion_finger_of_death_lua_stack:IsPurgable()
-	return false
+    return false
 end
 
 function modifier_lion_finger_of_death_lua_stack:RemoveOnDeath()
-	return false
+    return false
 end
 
 function modifier_lion_finger_of_death_lua_stack:GetAttributes()
-	return MODIFIER_ATTRIBUTE_PERMANENT
+    return MODIFIER_ATTRIBUTE_PERMANENT
 end
 
 --------------------------------------------------------------------------------
 -- Initializations
-function modifier_lion_finger_of_death_lua_stack:OnCreated( kv )
-	-- references
-	self:SetStackCount(1)
+function modifier_lion_finger_of_death_lua_stack:OnCreated(kv)
+    -- references
+    self:SetStackCount(1)
 end
 
-function modifier_lion_finger_of_death_lua_stack:OnRefresh( kv )
-	self:IncrementStackCount()
+function modifier_lion_finger_of_death_lua_stack:OnRefresh(kv)
+    if IsServer() then
+        self:IncrementStackCount()
+    end
 end
