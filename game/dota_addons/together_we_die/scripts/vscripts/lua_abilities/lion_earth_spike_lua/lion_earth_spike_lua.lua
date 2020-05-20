1de151d7110eeaf43987dc59df5eb3af8188cf1e
Woo Huiren <giantcrabby@gmail.com>
2020-05-20 23:07:57 +0800

    Revert earth spike to ground target

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lion_earth_spike_lua/lion_earth_spike_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lion_earth_spike_lua/lion_earth_spike_lua.lua
@@ -3,57 +3,40 @@ LinkLuaModifier( "modifier_generic_stunned_lua", "lua_abilities/generic/modifier
 LinkLuaModifier( "modifier_generic_knockback_lua", "lua_abilities/generic/modifier_generic_knockback_lua", LUA_MODIFIER_MOTION_BOTH )
 
 --------------------------------------------------------------------------------
--- AOE Radius
-function lion_earth_spike_lua:GetAOERadius()
-	return self:GetSpecialValueFor( "radius" )
-end
---------------------------------------------------------------------------------
 -- Ability Start
 function lion_earth_spike_lua:OnSpellStart()
-	-- unit identifier
 	local caster = self:GetCaster()
 	local target = self:GetCursorTarget()
-	local point = target:GetOrigin()
-	local search = self:GetSpecialValueFor( "radius" )
-	targets = FindUnitsInRadius(
-		caster:GetTeamNumber(),	-- int, your team number
-		point,	-- point, center point
-		nil,	-- handle, cacheUnit. (not known)
-		search,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-		DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-		DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE + DOTA_UNIT_TARGET_FLAG_NO_INVIS,	-- int, flag filter
-		0,	-- int, order filter
-		false	-- bool, can grow cache
-	)
+	local point = self:GetCursorPosition()
+
+	-- load data
+	if target then
+		point = target:GetOrigin()
+	end
 
 	local projectile_name = "particles/units/heroes/hero_lion/lion_spell_impale.vpcf"
+	local projectile_distance = self:GetCastRange( point, target )
 	local projectile_radius = self:GetSpecialValueFor( "width" )
 	local projectile_speed = self:GetSpecialValueFor( "speed" )
-	local caster_abs_origin = caster:GetAbsOrigin()
-	for _,enemy in pairs(targets) do
-		point = enemy:GetOrigin()
-		local projectile_distance = self:GetCastRange( point, enemy )
-		local projectile_direction = (point-caster:GetOrigin()):Normalized()
-		local info = {
-			Source = caster,
-			Ability = self,
-			vSpawnOrigin = caster_abs_origin,
-			
-			bDeleteOnHit = false,
-			
-			iUnitTargetTeam = DOTA_UNIT_TARGET_TEAM_ENEMY,
-			iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
-			iUnitTargetType = DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,
-			
-			EffectName = projectile_name,
-			fDistance = projectile_distance,
-			fStartRadius = projectile_radius,
-			fEndRadius = projectile_radius,
-			vVelocity = projectile_direction * projectile_speed,
-		}
-		ProjectileManager:CreateLinearProjectile(info)
-	end
+	local projectile_direction = (point-caster:GetOrigin()):Normalized()
+	local info = {
+		Source = caster,
+		Ability = self,
+		vSpawnOrigin = caster:GetAbsOrigin(),
+
+		bDeleteOnHit = false,
+
+		iUnitTargetTeam = self:GetAbilityTargetTeam(),
+		iUnitTargetFlags = DOTA_UNIT_TARGET_FLAG_NONE,
+		iUnitTargetType = self:GetAbilityTargetType(),
+
+		EffectName = projectile_name,
+		fDistance = projectile_distance,
+		fStartRadius = projectile_radius,
+		fEndRadius = projectile_radius,
+		vVelocity = projectile_direction * projectile_speed,
+	}
+	ProjectileManager:CreateLinearProjectile(info)
 
 	-- play effects
 	local sound_cast = "Hero_Lion.Impale"
@@ -68,14 +51,6 @@ function lion_earth_spike_lua:OnProjectileHit( target, location )
 
 	local stun = self:GetSpecialValueFor( "duration" )
 	local damage = self:GetAbilityDamage() + (self:GetCaster():GetIntellect() * self:GetSpecialValueFor("int_multiplier"))
-
-	local damageTable = {
-		victim = target,
-		attacker = self:GetCaster(),
-		damage = damage,
-		damage_type = self:GetAbilityDamageType(),
-		ability = self, --Optional.
-	}
 
 	-- stun
 	target:AddNewModifier(
