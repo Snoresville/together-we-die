94b8d047bfaf333834bc2f04f28355f475b64725
Woo Huiren <giantcrabby@gmail.com>
2020-05-21 17:46:14 +0800

    Rework burning spears

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/huskar_burning_spear_lua/huskar_burning_spear_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/huskar_burning_spear_lua/huskar_burning_spear_lua.lua
@@ -10,87 +10,93 @@ Ability checklist (erase if done/checked):
 ]]
 --------------------------------------------------------------------------------
 huskar_burning_spear_lua = class({})
-LinkLuaModifier( "modifier_huskar_burning_spear_lua", "lua_abilities/huskar_burning_spear_lua/modifier_huskar_burning_spear_lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_huskar_burning_spear_lua_stack", "lua_abilities/huskar_burning_spear_lua/modifier_huskar_burning_spear_lua_stack", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_generic_orb_effect_lua", "lua_abilities/generic/modifier_generic_orb_effect_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_huskar_burning_spear_lua", "lua_abilities/huskar_burning_spear_lua/modifier_huskar_burning_spear_lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_generic_orb_effect_lua", "lua_abilities/generic/modifier_generic_orb_effect_lua", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 -- Passive Modifier
 function huskar_burning_spear_lua:GetIntrinsicModifierName()
-	return "modifier_generic_orb_effect_lua"
+    return "modifier_generic_orb_effect_lua"
 end
 
 --------------------------------------------------------------------------------
+-- Ability Cast Filter
+function huskar_burning_spear_lua:CastFilterResultTarget(hTarget)
+    if not IsServer() then
+        return UF_SUCCESS
+    end
 
-function huskar_burning_spear_lua:CastFilterResultTarget( hTarget )
-	if IsServer() and hTarget ~= nil and hTarget.GetUnitName ~= nil then
-		return UnitFilter(
-					hTarget,
-					self:GetAbilityTargetTeam(),
-					self:GetAbilityTargetType(),
-					self:GetAbilityTargetFlags(),
-					self:GetCaster():GetTeamNumber()
-				)
-	end
-	return UF_FAIL_OTHER
+    local nResult = UnitFilter(
+            hTarget,
+            self:GetAbilityTargetTeam(),
+            self:GetAbilityTargetType(),
+            self:GetAbilityTargetFlags(),
+            self:GetCaster():GetTeamNumber()
+    )
+    if nResult ~= UF_SUCCESS then
+        return nResult
+    end
+
+    return UF_SUCCESS
 end
 
 -- AOE Radius
 function huskar_burning_spear_lua:GetAOERadius()
-	return self:GetSpecialValueFor( "radius" )
+    return self:GetSpecialValueFor("radius")
 end
 
 --------------------------------------------------------------------------------
 -- Orb Effects
 function huskar_burning_spear_lua:GetProjectileName()
-	return "particles/units/heroes/hero_huskar/huskar_burning_spear.vpcf"
+    return "particles/units/heroes/hero_huskar/huskar_burning_spear.vpcf"
 end
 
-function huskar_burning_spear_lua:OnOrbFire( params )
-	-- health cost
-	local damageTable = {
-		victim = self:GetCaster(),
-		attacker = self:GetCaster(),
-		damage = self:GetSpecialValueFor("health_cost") + (self:GetCaster():GetStrength() * self:GetSpecialValueFor( "str_multiplier" ) / 3),
-		damage_type = DAMAGE_TYPE_PURE,
-		ability = self, --Optional.
-		damage_flags = DOTA_DAMAGE_FLAG_NON_LETHAL + DOTA_DAMAGE_FLAG_HPLOSS + DOTA_DAMAGE_FLAG_NO_DAMAGE_MULTIPLIERS, --Optional.
-	}
-	ApplyDamage(damageTable)
+function huskar_burning_spear_lua:OnOrbFire(params)
+    local self_dmg_str_multiplier = self:GetSpecialValueFor("self_dmg_str_multiplier")
+    -- health cost
+    local damageTable = {
+        victim = self:GetCaster(),
+        attacker = self:GetCaster(),
+        damage = self:GetSpecialValueFor("health_cost") + (self:GetCaster():GetStrength() * self_dmg_str_multiplier),
+        damage_type = DAMAGE_TYPE_PURE,
+        ability = self, --Optional.
+        damage_flags = DOTA_DAMAGE_FLAG_NON_LETHAL + DOTA_DAMAGE_FLAG_HPLOSS + DOTA_DAMAGE_FLAG_NO_DAMAGE_MULTIPLIERS, --Optional.
+    }
+    ApplyDamage(damageTable)
 
-	-- play effects
-	local sound_cast = "Hero_Huskar.Burning_Spear.Cast"
-	EmitSoundOn( sound_cast, caster )
+    -- play effects
+    local sound_cast = "Hero_Huskar.Burning_Spear.Cast"
+    EmitSoundOn(sound_cast, caster)
 end
 
-function huskar_burning_spear_lua:OnOrbImpact( params )
-	local duration = self:GetDuration()
-	local caster = self:GetCaster()
-	local target = params.target
+function huskar_burning_spear_lua:OnOrbImpact(params)
+    local duration = self:GetDuration()
+    local caster = self:GetCaster()
+    local target = params.target
 
-	local search = self:GetSpecialValueFor( "radius" )
-	targets = FindUnitsInRadius(
-		caster:GetTeamNumber(),	-- int, your team number
-		target:GetOrigin(),	-- point, center point
-		nil,	-- handle, cacheUnit. (not known)
-		search,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-		DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-		DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-		0,	-- int, flag filter
-		0,	-- int, order filter
-		false	-- bool, can grow cache
-	)
+    local search = self:GetSpecialValueFor("radius")
+    local targets = FindUnitsInRadius(
+            caster:GetTeamNumber(), -- int, your team number
+            target:GetOrigin(), -- point, center point
+            nil, -- handle, cacheUnit. (not known)
+            search, -- float, radius. or use FIND_UNITS_EVERYWHERE
+            self:GetAbilityTargetTeam(), -- int, team filter
+            self:GetAbilityTargetType(), -- int, type filter
+            self:GetAbilityTargetFlags(), -- int, flag filter
+            0, -- int, order filter
+            false    -- bool, can grow cache
+    )
 
-	for _,enemy in pairs(targets) do
-		enemy:AddNewModifier(
-			caster, -- player source
-			self, -- ability source
-			"modifier_huskar_burning_spear_lua", -- modifier name
-			{ duration = duration } -- kv
-		)
-	end
+    for _, enemy in pairs(targets) do
+        enemy:AddNewModifier(
+                caster, -- player source
+                self, -- ability source
+                "modifier_huskar_burning_spear_lua", -- modifier name
+                { duration = duration } -- kv
+        )
+    end
 
-	-- play effects
-	local sound_cast = "Hero_Huskar.Burning_Spear"
-	EmitSoundOn( sound_cast, target )
+    -- play effects
+    local sound_cast = "Hero_Huskar.Burning_Spear"
+    EmitSoundOn(sound_cast, target)
 end
