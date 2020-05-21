71594853410cf027f2b71a6dc044328678c50719
Woo Huiren <giantcrabby@gmail.com>
2020-05-21 15:36:42 +0800

    Fix doom linken

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/doom_doom_lua/doom_doom_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/doom_doom_lua/doom_doom_lua.lua
@@ -10,48 +10,53 @@ Ability checklist (erase if done/checked):
 ]]
 --------------------------------------------------------------------------------
 doom_doom_lua = class({})
-LinkLuaModifier( "modifier_doom_doom_lua", "lua_abilities/doom_doom_lua/modifier_doom_doom_lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_doom_doom_lua", "lua_abilities/doom_doom_lua/modifier_doom_doom_lua", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 -- AOE Radius
 function doom_doom_lua:GetAOERadius()
-	return self:GetSpecialValueFor( "radius" )
+    return self:GetSpecialValueFor("radius")
 end
 -- Ability Start
 function doom_doom_lua:OnSpellStart()
-	-- unit identifier
-	local caster = self:GetCaster()
-	local target = self:GetCursorTarget()
-	local radius = self:GetSpecialValueFor( "radius" )
-	-- load data
-	local duration = self:GetSpecialValueFor( "duration" )
-	if caster:HasScepter() then
-		duration = self:GetSpecialValueFor( "duration_scepter" )
-	end
+    -- unit identifier
+    local caster = self:GetCaster()
+    local target = self:GetCursorTarget()
+    local radius = self:GetSpecialValueFor("radius")
+    -- load data
+    local duration = self:GetSpecialValueFor("duration")
+    if caster:HasScepter() then
+        duration = self:GetSpecialValueFor("duration_scepter")
+    end
 
-	-- Find Units in Radius
-	local targets = FindUnitsInRadius(
-			caster:GetTeamNumber(),	-- int, your team number
-			target:GetOrigin(),	-- point, center point
-			nil,	-- handle, cacheUnit. (not known)
-			radius,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-			DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-			DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES + DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE + DOTA_UNIT_TARGET_FLAG_NO_INVIS,	-- int, flag filter
-			0,	-- int, order filter
-			false	-- bool, can grow cache
-	)
+    -- cancel if linken
+    if target:TriggerSpellAbsorb(self) then
+        return
+    end
 
-	for _,enemy in pairs(targets) do
-		-- cancel if linken
-		if not enemy:TriggerSpellAbsorb( self ) then
-			-- add modifier
-			enemy:AddNewModifier(
-					caster, -- player source
-					self, -- ability source
-					"modifier_doom_doom_lua", -- modifier name
-					{ duration = duration } -- kv
-			)
-		end
-	end
+    -- Find Units in Radius
+    local targets = FindUnitsInRadius(
+            caster:GetTeamNumber(), -- int, your team number
+            target:GetOrigin(), -- point, center point
+            nil, -- handle, cacheUnit. (not known)
+            radius, -- float, radius. or use FIND_UNITS_EVERYWHERE
+            self:GetAbilityTargetTeam(), -- int, team filter
+            self:GetAbilityTargetType(), -- int, type filter
+            self:GetAbilityTargetFlags(), -- int, flag filter
+            0, -- int, order filter
+            false    -- bool, can grow cache
+    )
+
+    for _, enemy in pairs(targets) do
+        -- cancel if linken
+        if not enemy:TriggerSpellAbsorb(self) then
+            -- add modifier
+            enemy:AddNewModifier(
+                    caster, -- player source
+                    self, -- ability source
+                    "modifier_doom_doom_lua", -- modifier name
+                    { duration = duration } -- kv
+            )
+        end
+    end
 end
