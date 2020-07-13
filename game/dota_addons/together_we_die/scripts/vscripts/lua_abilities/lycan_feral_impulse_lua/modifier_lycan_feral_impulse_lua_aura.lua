dff6dd53de3397a7b59dc30d7aaed649f2e9aec9
Woo Huiren <giantcrabby@gmail.com>
2020-07-14 07:12:33 +0800

    Remove interval updating

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lycan_feral_impulse_lua/modifier_lycan_feral_impulse_lua_aura.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/lycan_feral_impulse_lua/modifier_lycan_feral_impulse_lua_aura.lua
@@ -3,45 +3,40 @@ modifier_lycan_feral_impulse_lua_aura = class({})
 --------------------------------------------------------------------------------
 -- Classifications
 function modifier_lycan_feral_impulse_lua_aura:IsHidden()
-	return false
+    return false
 end
 
 function modifier_lycan_feral_impulse_lua_aura:IsDebuff()
-	return false
+    return false
 end
 
 --------------------------------------------------------------------------------
 -- Initializations
-function modifier_lycan_feral_impulse_lua_aura:OnCreated( kv )
-	self.bonus_damage = self:GetAbility():GetSpecialValueFor( "bonus_damage" ) + self:GetCaster():GetStrength() * self:GetAbility():GetSpecialValueFor( "bonus_damage_increase_per_strength" )  -- special value
-	self.bonus_hp_regen = self:GetAbility():GetSpecialValueFor( "bonus_hp_regen" ) + self:GetCaster():GetStrength() * self:GetAbility():GetSpecialValueFor( "bonus_hp_regen_increase_per_strength" ) -- special value
-	self:StartIntervalThink(1)
+function modifier_lycan_feral_impulse_lua_aura:OnCreated(kv)
+    self.bonus_damage = self:GetAbility():GetSpecialValueFor("bonus_damage")
+    self.bonus_hp_regen = self:GetAbility():GetSpecialValueFor("bonus_hp_regen")
 end
 
-function modifier_lycan_feral_impulse_lua_aura:OnRefresh( kv)
-	self.bonus_damage = self:GetAbility():GetSpecialValueFor( "bonus_damage" ) + self:GetCaster():GetStrength() * self:GetAbility():GetSpecialValueFor( "bonus_damage_increase_per_strength" )  -- special value
-	self.bonus_hp_regen = self:GetAbility():GetSpecialValueFor( "bonus_hp_regen" ) + self:GetCaster():GetStrength() * self:GetAbility():GetSpecialValueFor( "bonus_hp_regen_increase_per_strength" ) -- special value
+function modifier_lycan_feral_impulse_lua_aura:OnRefresh(kv)
+    self.bonus_damage = self:GetAbility():GetSpecialValueFor("bonus_damage")
+    self.bonus_hp_regen = self:GetAbility():GetSpecialValueFor("bonus_hp_regen")
 end
 
 --------------------------------------------------------------------------------
 -- Modifier Effects
 function modifier_lycan_feral_impulse_lua_aura:DeclareFunctions()
-	local funcs = {
-		MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,
-		MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
-	}
+    local funcs = {
+        MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,
+        MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
+    }
 
-	return funcs
+    return funcs
 end
 
 function modifier_lycan_feral_impulse_lua_aura:GetModifierBaseDamageOutgoing_Percentage()
-	return self.bonus_damage
+    return self.bonus_damage + self:GetCaster():GetStrength() * self:GetAbility():GetSpecialValueFor("bonus_damage_increase_per_strength")
 end
 
 function modifier_lycan_feral_impulse_lua_aura:GetModifierConstantHealthRegen()
-	return self.bonus_hp_regen
-end
-
-function modifier_lycan_feral_impulse_lua_aura:OnIntervalThink()
-	self:ForceRefresh()
+    return self.bonus_hp_regen + self:GetCaster():GetStrength() * self:GetAbility():GetSpecialValueFor("bonus_hp_regen_increase_per_strength")
 end
