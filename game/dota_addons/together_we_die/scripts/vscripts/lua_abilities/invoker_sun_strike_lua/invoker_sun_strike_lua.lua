add8fecf0dd25c5359788a9e1788311e3ce5f57f
Woo Huiren <giantcrabby@gmail.com>
2020-07-17 05:02:24 +0800

    Fix sun strike visibility and add max count

--- game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/invoker_sun_strike_lua/invoker_sun_strike_lua.lua
+++ game/dota_addons/together_we_die/scripts/vscripts/lua_abilities/invoker_sun_strike_lua/invoker_sun_strike_lua.lua
@@ -1,20 +1,20 @@
 invoker_sun_strike_lua = class({})
-LinkLuaModifier( "modifier_invoker_sun_strike_lua_thinker", "lua_abilities/invoker_sun_strike_lua/modifier_invoker_sun_strike_lua_thinker", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_invoker_sun_strike_lua_thinker", "lua_abilities/invoker_sun_strike_lua/modifier_invoker_sun_strike_lua_thinker", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 --------------------------------------------------------------------------------
 function invoker_sun_strike_lua:GetBehavior()
-	if self:GetCaster():HasScepter() then
-		return DOTA_ABILITY_BEHAVIOR_NO_TARGET + DOTA_ABILITY_BEHAVIOR_IGNORE_BACKSWING
-	end
+    if self:GetCaster():HasScepter() then
+        return DOTA_ABILITY_BEHAVIOR_NO_TARGET + DOTA_ABILITY_BEHAVIOR_IGNORE_BACKSWING
+    end
 
-	return DOTA_ABILITY_BEHAVIOR_DIRECTIONAL + DOTA_ABILITY_BEHAVIOR_POINT + DOTA_ABILITY_BEHAVIOR_IGNORE_BACKSWING
+    return DOTA_ABILITY_BEHAVIOR_DIRECTIONAL + DOTA_ABILITY_BEHAVIOR_POINT + DOTA_ABILITY_BEHAVIOR_IGNORE_BACKSWING
 end
 --------------------------------------------------------------------------------
 -- Custom KV
 -- AOE Radius
 function invoker_sun_strike_lua:GetAOERadius()
-	return self:GetSpecialValueFor( "area_of_effect" )
+    return self:GetSpecialValueFor("area_of_effect")
 end
 
 -- Commented out for Cataclysm talent when available
@@ -59,46 +59,56 @@ end
 --------------------------------------------------------------------------------
 -- Ability Start
 function invoker_sun_strike_lua:OnSpellStart()
-	-- unit identifier
-	local caster = self:GetCaster()
+    -- unit identifier
+    local caster = self:GetCaster()
 
-	if self:GetCaster():HasScepter() then
-		local enemies = FindUnitsInRadius(
-			caster:GetTeamNumber(),	-- int, your team number
-			caster:GetOrigin(),	-- point, center point
-			nil,	-- handle, cacheUnit. (not known)
-			FIND_UNITS_EVERYWHERE,	-- float, radius. or use FIND_UNITS_EVERYWHERE
-			DOTA_UNIT_TARGET_TEAM_ENEMY,	-- int, team filter
-			DOTA_UNIT_TARGET_HERO + DOTA_UNIT_TARGET_BASIC,	-- int, type filter
-			DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,	-- int, flag filter
-			FIND_CLOSEST,	-- int, order filter
-			false	-- bool, can grow cache
-		)
+    if self:GetCaster():HasScepter() then
+        local enemies = FindUnitsInRadius(
+                caster:GetTeamNumber(), -- int, your team number
+                caster:GetOrigin(), -- point, center point
+                nil, -- handle, cacheUnit. (not known)
+                FIND_UNITS_EVERYWHERE, -- float, radius. or use FIND_UNITS_EVERYWHERE
+                self:GetAbilityTargetTeam(), -- int, team filter
+                self:GetAbilityTargetType(), -- int, type filter
+                self:GetAbilityTargetFlags(), -- int, flag filter
+                FIND_CLOSEST, -- int, order filter
+                false    -- bool, can grow cache
+        )
 
-		for _,enemy in pairs(enemies) do
-			self:SunStrikeLoc( caster, enemy:GetOrigin() )
-		end
-	else
-		self:SunStrikeLoc( caster, self:GetCursorPosition() )
-	end
+        local scepter_max_targets = self:GetSpecialValueFor("scepter_max_targets")
+        local count = 0
+        for _, enemy in pairs(enemies) do
+            self:SunStrikeLoc(caster, enemy:GetOrigin(), true)
+            count = count + 1
+
+            if count >= scepter_max_targets then
+                break
+            end
+        end
+    else
+        self:SunStrikeLoc(caster, self:GetCursorPosition(), false)
+    end
 
 end
 
-function invoker_sun_strike_lua:SunStrikeLoc( caster, point )
-	-- get values
-	local delay = self:GetSpecialValueFor("delay")
-	local vision_distance = self:GetSpecialValueFor("vision_distance")
-	local vision_duration = self:GetSpecialValueFor("vision_duration")
-	-- create modifier thinker
-	CreateModifierThinker(
-		caster,
-		self,
-		"modifier_invoker_sun_strike_lua_thinker",
-		{ duration = delay },
-		point,
-		caster:GetTeamNumber(),
-		false
-	)
-	-- create vision
-	AddFOWViewer( caster:GetTeamNumber(), point, vision_distance, vision_duration, false )
+function invoker_sun_strike_lua:SunStrikeLoc(caster, point, visible)
+    -- get values
+    local delay = self:GetSpecialValueFor("delay")
+    local vision_distance = self:GetSpecialValueFor("vision_distance")
+    local vision_duration = self:GetSpecialValueFor("vision_duration")
+    -- create modifier thinker
+    CreateModifierThinker(
+            caster,
+            self,
+            "modifier_invoker_sun_strike_lua_thinker",
+            {
+                duration = delay,
+                visible = visible
+            },
+            point,
+            caster:GetTeamNumber(),
+            false
+    )
+    -- create vision
+    AddFOWViewer(caster:GetTeamNumber(), point, vision_distance, vision_duration, false)
 end
